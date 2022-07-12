1f4d418e0afaabba2427ad81bb48c98e0f3df538
youngmoon1 <youngmoun525@gmail.com>
2022-07-12 17:41:50 +0900

    1

---
+++ And01_HelloWorld/app/src/main/java/com/example/and01_helloworld/StudentMain.java
@@ -0,0 +1,15 @@
+package com.example.and01_helloworld;
+
+public class StudentMain {
+    //주석
+    //변수선언
+    int num1  , num2;
+
+    public static void main(String[] args) {
+        //num1에 10담아보기
+        StudentMain sm = new StudentMain();
+        sm.num1 = 10;
+        //syso <-
+        System.out.println(sm.num1);
+    }
+}
